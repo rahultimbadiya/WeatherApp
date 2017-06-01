87d2450737a8a5256e05e3762d66092c8c9120f4
Rahul Timbadiya <timbadiyarahul@gmail.com>
2017-06-01 20:35:15 +0000

    change

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 '''
 DATABASES = {
     'default': dj_database_url.config(
-        default=config("HEROKU_POSTGRESQL_AMBER_URL"))
+        default="HEROKU_POSTGRESQL_AMBER_URL")
 }
 
 
