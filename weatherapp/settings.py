feef959cfa9e03f17d57528fbce4a40574125d5a
Rahul Timbadiya <timbadiyarahul@gmail.com>
2017-06-05 07:18:43 +0000

    commited

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -149,6 +149,6 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static'),]
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
